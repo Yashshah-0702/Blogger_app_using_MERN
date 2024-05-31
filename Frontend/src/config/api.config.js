42b6f25277fb0d68aea2a768d20ce5f011765320
yash shah <yash722002@gmail.com>
2024-05-31 17:09:00 +0530

    api_config_changed

--- Frontend/src/config/api.config.js
+++ Frontend/src/config/api.config.js
@@ -1,4 +1,4 @@
 module.exports={
-    apiKey:"http://localHost:7000"
-    // apiKey:"https://blogger-app-backend-764h.onrender.com"
+    // apiKey:"http://localHost:7000"
+    apiKey:"https://blogger-app-backend-764h.onrender.com"
 }
