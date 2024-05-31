f0cce19874ab00d299825a6a2b845ca7afe3b09b
yash shah <yash722002@gmail.com>
2024-05-31 12:35:03 +0530

    added:changes

--- Backend/src/configs/jwt.config.js
+++ Backend/src/configs/jwt.config.js
@@ -8,11 +8,7 @@ const jwtConfig = {
   |--------------------------------------------------------------------------
   */
   jwtSecret: process.env.JWT_SECRET,
-<<<<<<< HEAD
   tokenExpiration: process.env.TOKEN_EXPIRY,
-=======
-  tokenExpiration: process.env.TOKEN_EXPIRATION,
->>>>>>> b240a51fd190e94ed2920f845daf1c53b24a9c72
   resetPasswordSecret: process.env.RESET_PASSWORD_SECRET,
   resetPasswordTokenExpiration: process.env.RESET_PASSWORD_TOKEN_EXPIRATION,
 };
