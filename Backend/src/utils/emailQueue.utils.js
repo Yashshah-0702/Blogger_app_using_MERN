9b70a82104bf75826c7ec4313defcd2c75d91ffa
yash shah <yash722002@gmail.com>
2024-06-03 15:44:06 +0530

    added:some_changes

--- Backend/src/utils/emailQueue.utils.js
+++ Backend/src/utils/emailQueue.utils.js
@@ -2,11 +2,17 @@ const Queue = require("bull");
 const { sendPasswordToEmail } = require("./send-mail.utils");
 
 const emailQueue = new Queue("emailQueue", {
-  tls: true,
-  enableTLSForSentinelMode: false,
   redis: {
     host: "127.0.0.1",
     port: 6379,
+    tls: true,
+    enableTLSForSentinelMode: false,
+    maxRetriesPerRequest: null, // or a higher value like 100
+    retryStrategy: (times) => {
+      // Exponential backoff strategy
+      return Math.min(times * 50, 2000);
+    },
+    connectTimeout: 10000, // Increase connection timeout
   },
 });
 
